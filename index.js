ec3d31571e12e4c1d12d08d1b00a15a2aa29a698
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-05-18 11:24:17 -0700

    adds modal pop up

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackToken = process.env.SLACK_TOKEN;
 const PORT = process.env.SLACK_PORT || 3000;
 
+// const sortedQuestionsArray = [];
+
 // const slackEvents = createEventAdapter(slackSigningSecret);
 // const slackClient = new WebClient(slackToken);
 
@@ -22,15 +24,153 @@ const app = new App({
   logLevel: LogLevel.DEBUG,
 });
 
-app.action("static_select-action", async ({ ack, body, payload, say }) => {
+app.action("static_select-action", async ({ ack, body, payload, say, client }) => {
   await ack();
-  await say(
-    `Awesome! Let's start with 5 ${payload.selected_option.value} questions`
-  );
-  console.log("=======BODY=======", body);
+  // await say(`Awesome! Let's start with 5 ${payload.selected_option.value} questions`);
+  // console.log("=======BODY=======", body);
   console.log("=======PAYLOAD=======", payload);
   // getProblems(payload);
-  getRandomProblem(payload, 5);
+  let questionsArray = await getRandomProblem(payload, 5);
+  console.log('hurray hurray here is the questionsArray', questionsArray);
+  // startQuizz(body.trigger_id, 5)
+  try {
+    // Call views.open with the built-in client
+    const result = await client.views.open({
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: body.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              // "text": "How would you access the value of `x` in the following code? \n ```foo = { a: b, c: [ z, y, x ] }```"
+              "text": questionsArray[0].question,
+            },
+            "accessory": {
+              "type": "image",
+              "image_url": "https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island-300x300.png",
+              "alt_text": "calendar thumbnail"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": questionsArray[0].answers[0].answer_a,
+            },
+            "accessory": {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "A"
+              },
+              "value": "click_me_123"
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": questionsArray[0].answers[0].answer_b
+            },
+            "accessory": {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "B"
+              },
+              "value": "click_me_123"
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": questionsArray[0].answers[0].answer_c
+            },
+            "accessory": {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "C"
+              },
+              "value": "click_me_123"
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": 'questionsArray[0].answers[0].answer_d'
+            },
+            "accessory": {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "D"
+              },
+              "value": "click_me_123"
+            }
+          }
+          // {
+          //   type: 'section',
+          //   text: {
+          //     type: 'mrkdwn',
+          //     text: 'So here is your first question - better answer it correctly now'
+          //   },
+          //   accessory: {
+          //     type: 'button',
+          //     text: {
+          //       type: 'plain_text',
+          //       text: 'Click me!'
+          //     },
+          //     action_id: 'button_abc'
+          //   }
+          // },
+          // {
+          //   type: 'input',
+          //   block_id: 'input_c',
+          //   label: {
+          //     type: 'plain_text',
+          //     text: 'What are your hopes and dreams?'
+          //   },
+          //   element: {
+          //     type: 'plain_text_input',
+          //     action_id: 'dreamy_input',
+          //     multiline: true
+          //   }
+          // }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 
 async function getProblems(payload) {
@@ -39,7 +179,7 @@ async function getProblems(payload) {
     // const url = process.env.QUESTION_URL
     const questions = await axios.get(url);
     // return JSON.parse(questions.data)
-    console.log("get problems", questions.data);
+    // console.log("get problems", questions.data);
   } catch (e) {
     console.error(e);
   }
@@ -52,15 +192,23 @@ async function getRandomProblem(payload, num) {
       : `${process.env.QUESTION_URL}/search?category=${payload.selected_option.value}`;
   const questions = await axios.get(url);
   let qArr = questions.data;
-  let x = qArr.sort(() => Math.random() - Math.random()).slice(0, num);
-  console.log("getting qs", x);
-  // return value?
+  // let x = qArr.sort(() => Math.random() - Math.random()).slice(0, num);
+  let sortedQuestionsArray = qArr.sort(() => Math.random() - Math.random()).slice(0, num);
+  // console.log("getting qs", sortedQuestionsArray);
+  return sortedQuestionsArray
 }
-
+// async function startQuiz(message) {
+// }
 // Format imported random problems into questions with multiple choice
 
 app.message(async ({ message, say }) => {
   await callBot(message);
+});
+
+app.action('static_select-action', async ({ message, say, ack }) => {
+  // ack();
+  console.log('yay we are in app event ============================>>>>>>>>>>>>>>>', message);
+  // await startQuiz(message);
 });
 
 (async () => {
