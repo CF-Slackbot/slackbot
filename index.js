d20e91007aaae210e09c4464d7f40c62a0cc170e
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-05-18 17:00:52 -0700

    yay it works

--- index.js
+++ index.js
@@ -30,90 +30,6 @@ app.action(
     // console.log("=======PAYLOAD=======", payload);
     // questionsArray = await getRandomProblem(payload, 5);
     modalQs(ack,body,payload,client)
-    // try {
-    //   // Call views.open with the built-in client
-
-    //   await client.views.open({
-    //     // Pass a valid trigger_id within 3 seconds of receiving it
-    //     trigger_id: body.trigger_id,
-    //     // View payload
-    //     view: {
-    //       type: "modal",
-    //       // View identifier
-    //       callback_id: "view_1",
-    //       title: {
-    //         type: "plain_text",
-    //         text: "Modal title",
-    //       },
-    //       blocks: [
-    //         {
-    //           type: "divider",
-    //         },
-    //         {
-    //           type: "section",
-    //           text: {
-    //             type: "mrkdwn",
-    //             text: questionsArray[0].question,
-    //           },
-    //           accessory: {
-    //             type: "image",
-    //             image_url:
-    //               "https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island-300x300.png",
-    //             alt_text: "calendar thumbnail",
-    //           },
-    //         },
-    //         {
-    //           type: "divider",
-    //         },
-    //         {
-    //           type: "input",
-    //           block_id: "input_block",
-    //           element: {
-    //             type: "radio_buttons",
-    //             options: [
-    //               {
-    //                 text: {
-    //                   type: "plain_text",
-    //                   text: questionsArray[0].answers[0].answer_a,
-    //                   emoji: true,
-    //                 },
-    //                 value: "answer_a",
-    //               },
-    //               {
-    //                 text: {
-    //                   type: "plain_text",
-    //                   text: questionsArray[0].answers[0].answer_b,
-    //                   emoji: true,
-    //                 },
-    //                 value: "answer_b",
-    //               },
-    //               {
-    //                 text: {
-    //                   type: "plain_text",
-    //                   text: questionsArray[0].answers[0].answer_c,
-    //                   emoji: true,
-    //                 },
-    //                 value: "answer_c",
-    //               },
-    //             ],
-    //             action_id: "radio_buttons-action",
-    //           },
-    //           label: {
-    //             type: "plain_text",
-    //             text: "Label",
-    //             emoji: true,
-    //           },
-    //         },
-    //       ],
-    //       submit: {
-    //         type: "plain_text",
-    //         text: "Submit",
-    //       },
-    //     },
-    //   });
-    // } catch (error) {
-    //   console.error(error);
-    // }
   }
 );
 
@@ -127,31 +43,9 @@ app.view("view_1", async ({ ack, body, view, client }) => {
     ]["value"];
   let ans = questionsArray.shift()["correct_answer"];
 
-  if (val === ans) {
-    await client.chat.postMessage({
-      channel: user,
-      text: "You got it right!",
-    });
-  } else {
-    await client.chat.postMessage({
-      channel: user,
-      text: "Better luck next time!",
-    });
-  }
+
 });
 
-
-async function getProblems(payload) {
-  try {
-    const url = `${process.env.QUESTION_URL}?category=${payload.selected_option.value}`;
-    // const url = process.env.QUESTION_URL
-    const questions = await axios.get(url);
-    // return JSON.parse(questions.data)
-    // console.log("get problems", questions.data);
-  } catch (e) {
-    console.error(e);
-  }
-}
 
 async function getRandomProblem(payload, num) {
   const url =
